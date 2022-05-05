5bb5cf4dfcc316c149b4f1bad750dd660bae4dc3
dbsdm <denys.borsukov@gmail.com>
2022-05-05 22:11:02 +0000

    minor fix

--- nvim/lua/nvimtree.lua
+++ nvim/lua/nvimtree.lua
@@ -1,4 +1,4 @@
-require'nvim-tree'.setup { 
+require'nvim-tree'.setup {
   auto_reload_on_write = true,
   disable_netrw = false,
   hijack_cursor = false,
@@ -109,4 +109,4 @@ require'nvim-tree'.setup {
       profile = false,
     },
   },
-} 
+}
