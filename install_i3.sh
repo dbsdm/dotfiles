f0ba31522022b45a4fbfe04c3dfd3f7fc005d616
dbsdm <denys.borsukov@gmail.com>
2022-05-06 08:02:27 +0000

    init

---
+++ install_i3.sh
@@ -0,0 +1,13 @@
+
+LINK(){
+    sudo rm -rf $2/$1
+    sudo ln -sf $(pwd)/$1 $2
+}
+
+# install KDE Plasma
+sudo pacman -S --noconfirm - < pacman.i3.txt
+sudo yay -S --noconfirm - < yay.i3.txt
+sudo systemctl enable sddm
+LINK "i3" "$HOME/.config"
+LINK "picom.conf" "$HOME/.config"
+LINK "polybar" "$HOME/.config"
