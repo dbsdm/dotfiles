d628085903722cbcad3f07decb969c30ae4b1c5f
dbsdm <denys.borsukov@gmail.com>
2022-05-07 01:59:11 +0000

    rofi

--- install_i3.sh
+++ install_i3.sh
@@ -6,8 +6,9 @@ LINK(){
 
 # install KDE Plasma
 sudo pacman -S --noconfirm - < pacman.i3.txt
-sudo yay -S --noconfirm - < yay.i3.txt
+yay -S --noconfirm - < yay.i3.txt
 sudo systemctl enable sddm
 LINK "i3" "$HOME/.config"
 LINK "picom.conf" "$HOME/.config"
 LINK "polybar" "$HOME/.config"
+LINK "rofi" "$HOME/.config"
