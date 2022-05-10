0c118eb78fbf26c3e4e4eab898139c1472d06fb1
Denis Borsukov <denys.borsukov@gmail.com>
2022-05-10 10:38:09 +0000

    packer bootstrap and automatic sync on save

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,47 @@
+----------------------------------- SETUP ------------------------------------
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call to avoid errors on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
+}
+
+----------------------------------- PLUGINS ----------------------------------
+
 return require("packer").startup(function()
 
     ------------------------------- GENERAL ----------------------------------
@@ -101,4 +145,8 @@ return require("packer").startup(function()
         requires = "L3MON4D3/LuaSnip"
     }
 
+    -------------------- Sync After Bootstraping Packer ----------------------
+    if PACKER_BOOTSTRAP then
+        require('packer').sync()
+    end
 end)
