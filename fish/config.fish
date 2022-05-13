3842acef7543b25bafb8199e61ae0bd0e471ffcc
Denis Borsukov <denys.borsukov@gmail.com>
2022-05-13 20:31:28 +0000

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x VIRTUAL_ENV_DISABLE_PROMPT 0
 
 alias fish_aliases="nvim ~/.config/fish/config.fish"
 alias ll="exa -lha --git"
