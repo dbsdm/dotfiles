48896088c9f89f32c3e1536fa32233129b456b99
Denis Borsukov <denys.borsukov@gmail.com>
2022-05-17 18:14:43 +0300

    Minor

--- desktops/i3/install.sh
+++ desktops/i3/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+LINK(){
+    sudo rm -rf $2/$1
+    sudo ln -sf $(pwd)/$1 $2
+}
+
 echo -e "\n\e[31mInstalling i3gaps\e[0m" 
 sudo pacman -S --noconfirm - < pacman.txt
 yay -S --noconfirm - < yay.txt
