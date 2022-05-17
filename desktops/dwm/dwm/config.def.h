0aa04e09c69ca365dfa9a054b555230fa271d55d
Denis Borsukov <denys.borsukov@gmail.com>
2022-05-18 01:06:34 +0300

    Cleaned up dwm

--- desktops/dwm/dwm/config.def.h
+++ desktops/dwm/dwm/config.def.h
@@ -105,35 +105,23 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|Mod1Mask,              XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|Mod1Mask,              XK_d,      incnmaster,     {.i = -1 } },
-	/* { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, */
-	/* { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, */
-	/* { MODKEY|ShiftMask,             XK_Return, zoom,           {0} }, */
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|Mod1Mask,              XK_space,  setlayout,      {0} },
+	{ MODKEY,                       XK_q,      setlayout,      {0} },
 	{ MODKEY,                       XK_f,      togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	/* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
-	/* { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
-	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
-	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
-	/* { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, */
-	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
 	{ MODKEY,                       XK_Right,  viewnext,       {0} },
 	{ MODKEY,                       XK_Left,   viewprev,       {0} },
 	{ MODKEY,                       XK_Tab,    viewnext,       {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    viewprev,       {0} },
-	/* { MODKEY,                       XK_Tab,    tagtonext,      {0} }, */
-	/* { MODKEY|ShiftMask,             XK_Tab,    tagtoprev,      {0} }, */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
