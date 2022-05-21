7631a45ef7c415c9692720dfedc9bc2b072d25e6
Denis Borsukov <denys.borsukov@gmail.com>
2022-05-21 08:35:16 +0300

    Done plugin 5000ms trigger

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set VISUAL "nvim"
 set -x VIRTUAL_ENV_DISABLE_PROMPT 0
 set -gx QT_QPA_PLATFORMTHEME qt5ct
 set -gx PATH "$HOME/.cargo/bin" $PATH
+# plugin confs
+set -U __done_min_cmd_duration 5000 #ms
 
 alias fish_aliases="nvim ~/.config/fish/config.fish"
 alias ll="exa -lha --git"
