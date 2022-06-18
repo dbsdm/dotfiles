e266370f4f9ae57be9fd53e4aa4a71bf46ec6fbf
Denis Borsukov <denys.borsukov@gmail.com>
2022-06-18 22:02:34 +0300

    new informative greeting

--- configurations/minimal/fish/config.fish
+++ configurations/minimal/fish/config.fish
@@ -1,15 +1,11 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-set fish_greeting
+fish_vi_key_bindings
 set EDITOR "nvim"    
 set VISUAL "nvim" 
 set -x VIRTUAL_ENV_DISABLE_PROMPT 0
 set -gx QT_QPA_PLATFORMTHEME qt5ct
 set -gx PATH "$HOME/.cargo/bin" $PATH
 set -gx PATH "$HOME/.local/bin" $PATH
-# plugin confs
-set --universal pure_enable_git true
+set -U pure_enable_git true
 set -U __done_min_cmd_duration 5000 #ms
 
 alias fish_aliases="nvim ~/.config/fish/config.fish"
