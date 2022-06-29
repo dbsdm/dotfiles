4835cdf65b60ac603b5880cc839a1ea7dd0f7825
Denis Borsukov <denys.borsukov@gmail.com>
2022-06-29 20:16:14 +0300

    unclutter $HOME by following XDG specifications for applications

--- configurations/minimal/fish/config.fish
+++ configurations/minimal/fish/config.fish
@@ -14,8 +14,17 @@ set VISUAL "nvim"
 # set -gx QT_QPA_PLATFORM wayland
 set -gx QT_QPA_PLATFORMTHEME qt5ct
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 0
-set -gx PATH "$HOME/.cargo/bin" $PATH
-set -gx PATH "$HOME/.local/bin" $PATH
+set -gx PATH "$HOME/.cargo/bin/" $PATH
+set -gx PATH "$HOME/.local/bin/" $PATH
+
+##############################################################################
+#                                    XDG                                     #
+##############################################################################
+
+set -Ux XDG_DATA_HOME $HOME/.local/share/
+set -Ux XDG_CONFIG_HOME $HOME/.config/
+set -Ux XDG_STATE_HOME $HOME/.local/state/
+set -Ux XDG_CACHE_HOME $HOME/.cache/
 
 ##############################################################################
 #                              PLUGINS SETTINGS                              #
